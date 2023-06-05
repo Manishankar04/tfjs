a3eacf56cc571f89fa15f6795191f54b66f6c023
ManishankarSridhar <es.maniobu@gmail.com>
2023-06-05 10:32:26 +0530

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ],
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
+};
